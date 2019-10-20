6070fe823d4cdbaabd617d49c5253e8ece7e7be1
me0830code <me0830code@gmail.com>
2019-10-20 13:40:53 +0800

    [Start] Start MoneyExchange Project

---
+++ MoneyExchange/Controller/MoneyExchangeVC.swift
@@ -0,0 +1,22 @@
+//
+//  MoneyExchangeVC.swift
+//  MoneyExchange
+//
+//  Created by Chien on 2019/10/20.
+//  Copyright © 2019 Chien. All rights reserved.
+//
+
+import UIKit
+
+
+class MoneyExchangeVC: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Hello")
+    }
+    
+    
+}
